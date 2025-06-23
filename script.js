ebff249e8f8df0cdc2d7d7d05b14192ba1412f63
William Harkley <56347104+WillHarkley@users.noreply.github.com>
2025-06-23 14:26:54 -0400

    Update script.js
    Added dark mode function

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.getElementById('consult-form').addEventListener('submit', function (e) 
     e.preventDefault();
     document.getElementById('response').textContent = 'Thanks for reaching out! I’ll get back to you soon.';
   });
+
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+}
